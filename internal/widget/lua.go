537e1386c1b56ebf39800bef0a6355c3e397cc30
Borna Lang <borna.lang@gmail.com>
2023-06-24 21:28:20 +0200

    More improvements

--- internal/widget/lua.go
+++ internal/widget/lua.go
@@ -1,11 +1,14 @@
 package widget
 
 import (
+	"fmt"
+	"reflect"
 	"errors"
 
 	"github.com/yuin/gopher-lua"
 	"layeh.com/gopher-luar"
 	"github.com/fsnotify/fsnotify"
+	"github.com/veandco/go-sdl2/sdl"
 
 	"github.com/glupi-borna/wiggo/internal/ui"
 	"github.com/glupi-borna/wiggo/internal/platform"
@@ -44,6 +47,57 @@ func getLuaFn(l *lua.LState, name string, required bool) (*lua.LFunction, error)
 	return fn, nil
 }
 
+func luaColor(val lua.LValue) (sdl.Color, bool) {
+	if val == lua.LNil { return sdl.Color{}, true }
+	num, ok := val.(lua.LNumber)
+	if ok { return ui.ColHex(uint32(num)), true }
+
+	ud, ok := val.(*lua.LUserData)
+	if ok {
+		c, ok := ud.Value.(sdl.Color)
+		if ok { return c, true }
+		println("Failed to parse", reflect.ValueOf(c).String())
+	}
+
+	return sdl.Color{}, false
+}
+
+func luaFloat32(val lua.LValue) (float32, bool) {
+	if val == lua.LNil { return 0, true }
+	num, ok := val.(lua.LNumber)
+	if ok { return float32(num), true }
+	return 0, false
+}
+
+func luaStyleVar[K any](val lua.LValue, convert func(lua.LValue)(K, bool)) ui.StyleVariant[K] {
+	var sv ui.StyleVariant[K]
+	v, ok := convert(val)
+	if ok { return ui.StyleVar(v) }
+
+	tbl, ok := val.(*lua.LTable)
+	if ok {
+		normal, ok := convert(tbl.RawGetString("Normal"))
+		sv.Normal = normal
+		if !ok { println("Normal: unsupported value") }
+
+		active, ok := convert(tbl.RawGetString("Active"))
+		sv.Active = active
+		if !ok {
+			println("Active: unsupported value")
+			sv.Active = normal
+		}
+
+		hot, ok := convert(tbl.RawGetString("Hot"))
+		sv.Hot = hot
+		if !ok {
+			println("Hot: unsupported value")
+			sv.Hot = active
+		}
+	}
+
+	return sv
+}
+
 func (lw *LuaWidget) Name() string { return lw.name }
 func (lw *LuaWidget) Path() string { return lw.path }
 func (lw *LuaWidget) Type() string { return "lua" }
@@ -67,6 +121,11 @@ func (lw *LuaWidget) init() error {
 	lw.l.SetGlobal("Button", luar.New(lw.l, ui.Button))
 	lw.l.SetGlobal("Slider", luar.New(lw.l, ui.Slider))
 	lw.l.SetGlobal("Invisible", luar.New(lw.l, ui.Invisible))
+	lw.l.SetGlobal("Col", luar.New(lw.l, ui.Col))
+	lw.l.SetGlobal("RGBA", luar.New(lw.l, func (r, g, b, a uint8) sdl.Color {
+		return sdl.Color{r, g, b, a}
+	}))
+	lw.l.SetGlobal("ColHex", luar.New(lw.l, ui.ColHex))
 
 	NodeIter := func (iterable *ui.Node, item *ui.Node) *ui.Node {
 		if item == nil { return iterable }
@@ -87,6 +146,50 @@ func (lw *LuaWidget) init() error {
 	lw.l.SetGlobal("ChildrenSize", luar.New(lw.l, ui.ChildrenSize))
 	lw.l.SetGlobal("FitText", luar.New(lw.l, ui.FitText))
 	lw.l.SetGlobal("Close", luar.New(lw.l, platform.Platform.Close))
+
+	lw.l.SetGlobal("Padding", luar.New(lw.l, func(args ...lua.LNumber) ui.Padding {
+		if len(args) == 1 { return ui.Padding1(float32(args[0])) }
+		if len(args) == 2 { return ui.Padding2(float32(args[0]), float32(args[1])) }
+		if len(args) == 4 {
+			return ui.Padding{
+				Left: float32(args[0]),
+				Top: float32(args[1]),
+				Right: float32(args[2]),
+				Bottom: float32(args[3]),
+			}
+		}
+		println("Padding(): unsupported number of arguments:", len(args))
+		return ui.Padding{}
+	}))
+
+	lw.l.SetGlobal("Style", luar.New(lw.l, func(arg *lua.LTable) *ui.Style {
+		s := ui.DefaultStyle.Copy()
+
+		bg := arg.RawGetString("Background")
+		if bg != lua.LNil { s.Background = luaStyleVar(bg, luaColor) }
+
+		fg := arg.RawGetString("Foreground")
+		if fg != lua.LNil { s.Foreground = luaStyleVar(fg, luaColor) }
+
+		bo := arg.RawGetString("Border")
+		if bo != lua.LNil { s.Border = luaStyleVar(bo, luaColor) }
+
+		cr := arg.RawGetString("CornerRadius")
+		if cr != lua.LNil { s.CornerRadius = luaStyleVar(cr, luaFloat32) }
+
+		pd := arg.RawGetString("Padding")
+		if pd != lua.LNil {
+			pdud, ok := pd.(*lua.LUserData)
+			if ok {
+				p, ok := pdud.Value.(ui.Padding)
+				if ok {
+					s.Padding = p
+				}
+			}
+		}
+
+		return s
+	}))
 
 	initfn, err := getLuaFn(lw.l, "init", false)
 	if err != nil { return err }
@@ -136,15 +239,23 @@ func (lw *LuaWidget) Init() error {
 	return nil
 }
 
-func (lw *LuaWidget) CallFn(fn *lua.LFunction, args ...lua.LValue) (lua.LValue, error) {
+func (lw *LuaWidget) CallFn(fn *lua.LFunction, args ...lua.LValue) (ret lua.LValue, err error) {
+	defer func() {
+		panic_val := recover()
+		if panic_val != nil {
+			ret = nil
+			err = errors.New(fmt.Sprint(panic_val))
+		}
+	}()
+
 	if fn == nil { return nil, nil }
-	err := lw.l.CallByParam(lua.P{
+	err = lw.l.CallByParam(lua.P{
 		Fn: fn,
 		NRet: 1,
-		Protect: false,
+		Protect: true,
 	}, args...)
 	if err != nil { return nil, err }
-	ret := lw.l.Get(-1)
+	ret = lw.l.Get(-1)
 	lw.l.Pop(1)
 	return ret, nil
 }
--- wiggo_test.lua
+++ wiggo_test.lua
@@ -1,16 +1,16 @@
 local radius = 0
 
 function SliderWidget()
-        if TextButton("-") then radius = radius - 1 end
-        Invisible(Px(8))
+    if TextButton("-") then radius = radius - 1 end
+    Invisible(Px(8))
 
-        radius = Slider(radius, 0, 32)
-        local slider = UI().Last
-        slider.Size.W = Fr(1)
-        slider.Size.H = Px(30)
+    radius = Slider(radius, 0, 32)
+    local slider = UI().Last
+    slider.Size.W = Fr(1)
+    slider.Size.H = Px(30)
 
-        Invisible(Px(8))
-        if TextButton("+") then radius = radius + 1 end
+    Invisible(Px(8))
+    if TextButton("+") then radius = radius + 1 end
 end
 
 function CloseRow()
@@ -20,9 +20,14 @@ function CloseRow()
 end
 
 function frame()
-    UI().Root.Style.CornerRadius.Normal = Animate(radius, "radius")
+    UI().Root.Style = Style{
+        Background = RGBA(255, 0, 0, 255),
+        CornerRadius = Animate(radius, "radius")
+    }
 
-    Text("Corner radius = " .. tostring(math.floor(radius+0.5)))
+    Text("Corner radius = " .. tostring(math.floor(radius+0.5))).Style = Style{
+        Foreground = ColHex(0xffffffff)
+    }
 
     for n in With(Row()) do
         n.Size.W = Fr(1)
